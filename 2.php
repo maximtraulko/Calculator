daa16e82f6ccd9442107e39c01a2d3511a515e51
maximtraulko <max.traulko@bk.ru>
2016-10-09 21:58:49 +0700

    Калькулятор для сложения и вычитания чисел

---
+++ 2.php
@@ -0,0 +1,30 @@
+<html>
+	<HEAD>
+		<meta charset ="UTF-8">
+		<TITLE>Текстовый документ 00001</title>
+	</HEAD>
+	<body>
+		<form action="2.php" method="GET">
+		<input type="text" name="arg1" value="<?=$_GET['arg1']?>" />
+		<input type="text" name="arg2" value="<?=$_GET['arg2']?>" />
+		<input type="submit" name='add' value="Сумма">
+		<input type="submit" name='sub' value="Выч">
+		</form>
+		<?php
+			if (isset($_GET['add'])){
+			if (isset ($_GET ['arg1']) &&isset($_GET['arg2'])){
+			echo intval($_GET['arg1'])+intval($_GET['arg2']);
+			$_GET['arg1'];
+			$_GET['arg1'];
+			}	
+		}
+		if (isset ($_GET ['sub'])){
+			if (isset($_GET['arg1']) &&isset($_GET['arg2'])) {
+				echo intval ($_GET['arg1'])-intval($_GET['arg2']);
+				$_GET['arg1'];
+				$_GET['arg1'];
+			}	
+		}
+		?>
+	</body>
+</html>
